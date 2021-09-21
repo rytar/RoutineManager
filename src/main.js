f8f5dbe344af9335bf075053faf1f038cd009f95
rytaryu <53173674+rytar@users.noreply.github.com>
2021-09-21 14:05:02 +0900

    first code

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+const createWindow = () => {
+
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        width: 800,
+        height: 600
+    });
+
+    mainWindow.loadFile("index.html");
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform != "darwin") app.quit();
+});
+
+app.on("activate", () => {
+    if (mainWindow == null) createWindow();
+});
