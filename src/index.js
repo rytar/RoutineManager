1af155b38975e1fc8c9c5a40263f20d3afb214a4
rytaryu <53173674+rytar@users.noreply.github.com>
2021-09-23 15:49:46 +0900

    add some tools

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
 const EditorJS = require("@editorjs/editorjs");
 const Header = require("@editorjs/header");
+const List = require("@editorjs/list");
+const NestedList = require("@editorjs/nested-list");
 const CheckList = require("@editorjs/checklist");
+const Code = require("@editorjs/code");
+const InlineCode = require("@editorjs/inline-code");
+const Link = require("@editorjs/link");
+const Delimiter = require("@editorjs/delimiter");
+const Marker = require("@editorjs/marker");
+const Underline = require("@editorjs/underline");
+const Warning = require("@editorjs/warning");
+const SimpleImage = require("@editorjs/simple-image");
+const Table = require("@editorjs/table");
 const { ipcRenderer } = require("electron");
 
 ipcRenderer.invoke("get-contents").then((result) => {
     const contents = result;
 
+    const InlineTools = ["inlineCode", "quote", "link", "marker", "underline"];
+
     const editor = new EditorJS({
         holder: "editorjs",
         tools: {
             header: Header,
+            list: {
+                class: List,
+                inlineToolBar: InlineTools
+            },
+            nestedlist: {
+                class: NestedList,
+                inlineToolBar: InlineTools
+            },
             checklist: {
                 class: CheckList,
-                inlineToolBar: true
-            }
+                inlineToolBar: InlineTools
+            },
+            code: {
+                class: Code,
+                inlineToolBar: InlineTools
+            },
+            inlinecode: InlineCode,
+            link: Link,
+            delimiter: Delimiter,
+            marker: Marker,
+            underline: Underline,
+            warning: {
+                class: Warning,
+                inlineToolBar: InlineTools
+            },
+            simpleimage: SimpleImage,
+            table: Table
         },
         data: contents,
         onChange: () => {
